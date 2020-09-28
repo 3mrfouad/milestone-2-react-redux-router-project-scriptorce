0c115948fd9c536f5afb6f65c242878c03a1ae02
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-09-28 12:42:18 -0600

    No changes dones -Puled master ;

--- src/components/Shopping-cart.js
+++ src/components/Shopping-cart.js
@@ -69,88 +69,88 @@ const ShoppingCartComponent = () => {
         <form id="shopping-cart" onSubmit={SubmitForm}>
           <h1>Your Order</h1>
           <ul>
-          {inCartProducts.map((inCartProduct) => {
-            subTotal = subTotal + inCartProduct.price * inCartProduct.inCartQty;
-            return (
-           
-              <li key={inCartProduct.id}>
-                <section id="cart-items">
-                  <div className="container">
-                    {/* <h2>Product</h2> */}
-                    <h3>{inCartProduct.title}</h3>
-                    <img
-                      id="pdt-img"
-                      src={inCartProduct.image}
-                      alt="Product in shopping cart"
-                    />
-                    <section id="text-container">
-                      <p id="description">{inCartProduct.description}</p>
-                      <p id="price">$ {inCartProduct.price}</p>
-                    </section>
-                  </div>
-                  <div id="qty-status">
-                    <div id="cart-qty-style">
+            {inCartProducts.map((inCartProduct) => {
+              subTotal =
+                subTotal + inCartProduct.price * inCartProduct.inCartQty;
+              return (
+                <li key={inCartProduct.id}>
+                  <section id="cart-items">
+                    <div className="container">
+                      {/* <h2>Product</h2> */}
+                      <h3>{inCartProduct.title}</h3>
+                      <img
+                        id="pdt-img"
+                        src={inCartProduct.image}
+                        alt="Product in shopping cart"
+                      />
+                      <section id="text-container">
+                        <p id="description">{inCartProduct.description}</p>
+                        <p id="price">$ {inCartProduct.price}</p>
+                      </section>
+                    </div>
+                    <div id="qty-status">
+                      <div id="cart-qty-style">
+                        <span>
+                          <button
+                            className="plus"
+                            type="button"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              dispatch(IncreaseCartQty(inCartProduct.id));
+                            }}
+                          >
+                            &#43; {/* Plus sign */}
+                          </button>
+                          &nbsp;
+                        </span>
+                        <span id="qty">
+                          <input
+                            type="number"
+                            value={inCartProduct.inCartQty}
+                            min="0"
+                            onChange={(e) => {
+                              e.target.value > -1 || (e.target.value = "0");
+                              if (e.target.value > -1) {
+                                dispatch(
+                                  SetCartQty(inCartProduct.id, e.target.value)
+                                );
+                              }
+                            }}
+                          ></input>
+                          &nbsp;
+                        </span>
+                        <span>
+                          <button
+                            className="minus"
+                            type="button"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              dispatch(DecreaseCartQty(inCartProduct.id));
+                            }}
+                          >
+                            &#8722; {/* Minus sign */}
+                          </button>
+                          &nbsp;{" "}
+                        </span>
+                      </div>
                       <span>
                         <button
-                          className="plus"
                           type="button"
-                          onClick={(e) => {
-                            e.preventDefault();
-                            dispatch(IncreaseCartQty(inCartProduct.id));
+                          className="rmv-btn"
+                          onClick={() => {
+                            dispatch(RemoveFromCart(inCartProduct.id));
                           }}
                         >
-                          &#43; {/* Plus sign */}
+                          Remove From Cart
                         </button>
                         &nbsp;
-                      </span>
-                      <span id="qty">
-                        <input
-                          type="number"
-                          value={inCartProduct.inCartQty}
-                          min="0"
-                          onChange={(e) => {
-                            e.target.value > -1 || (e.target.value = "0");
-                            if (e.target.value > -1) {
-                              dispatch(
-                                SetCartQty(inCartProduct.id, e.target.value)
-                              );
-                            }
-                          }}
-                        ></input>
-                        &nbsp;
-                      </span>
-                      <span>
-                        <button
-                          className="minus"
-                          type="button"
-                          onClick={(e) => {
-                            e.preventDefault();
-                            dispatch(DecreaseCartQty(inCartProduct.id));
-                          }}
-                        >
-                          &#8722; {/* Minus sign */}
-                        </button>
-                        &nbsp;{" "}
                       </span>
                     </div>
-                    <span>
-                      <button
-                        type="button"
-                        className="rmv-btn"
-                        onClick={() => {
-                          dispatch(RemoveFromCart(inCartProduct.id));
-                        }}
-                      >
-                        Remove From Cart
-                      </button>
-                      &nbsp;
-                    </span>
-                  </div>
-                </section>
-              </li>
-            );
-           
-          })} </ul>
+                  </section>
+                </li>
+              );
+            })}{" "}
+          </ul>
           <section id="items-pickup">
             <button id="btn-back">
               <Link to="/shop">Back To Shopping</Link>
